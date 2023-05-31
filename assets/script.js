88aa00ae0c12d6480f220ccda8bcd64c014a54a8
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-31 10:19:51 -0500

    Update script.js
    added query selectors for buttons and cards

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Housekeeping
+    const citySearchForm = document.querySelector(".city-search-form");
+    const cityButtons = document.getElementById("city-buttons");
+    const weatherCard = document.getElementById("weather-data").querySelector(".card-body");
+    const forecast = document.querySelector(".card-group");
+});
+
+const apiKey = "ce3d08cfda63c419bc1f505bd8fd502f";
